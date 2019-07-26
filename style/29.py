d8e8546e2d501c1fb0a59f5bdafd778e9c408820
heyu <shy19940918@live.com>
2019-07-26 11:45:08 +0800

    with 上下文管理器

---
+++ style/29.py
@@ -0,0 +1,60 @@
+from contextlib import contextmanager
+
+
+class FileManager:
+
+    def __init__(self, name, mode):
+        print('init')
+        self.name = name
+        self.mode = mode
+
+    def __enter__(self):
+        print('enter')
+        self.file = open(self.name, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('exit')
+        if self.file:
+            self.file.close()
+
+
+# with FileManager('d:/aaa.txt', 'w') as f:
+#     print('ready')
+#     f.write('hello word')
+
+
+class Foo:
+
+    def __init__(self):
+        print('init')
+
+    def __enter__(self):
+        print('enter')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('exit')
+        if exc_type:
+            print(f'exec_type: {exc_type}')
+            print(f'exc_val: {exc_val}')
+            print(f'exc_tb: {exc_tb}')
+            print('exception handle')
+        return True
+
+
+# with Foo() as f:
+#     # raise Exception('xxxx')
+#     print('xxx')
+
+@contextmanager
+def file_manage(name, mode):
+    try:
+        f = open(name, mode)
+        yield f
+    finally:
+        f.close()
+
+
+with file_manage('d:/aaa.txt', 'w') as h:
+    h.write('hello word')
